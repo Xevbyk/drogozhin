1c26a472551617ffb85a2f71e452b6c6f01fc18e
Xevbyk <98812493+Xevbyk@users.noreply.github.com>
2023-02-14 10:00:37 +0300

    Классная работа 2

---
+++ Задание 2.py
@@ -0,0 +1,18 @@
+a = 10
+b = 25
+# 2 задание 1 пункт
+print(a < 11 and b > 11)
+print(a > 2 and b > 2)
+# 2 пункт
+print(a > 12 and b > 26)
+print(a > 254 and b > 2 )
+# 3 задание
+print(a < 11 or b < 11)
+print(a > 11 or b > 11)
+print(a > 1222 or b > 226)
+print(a > 1212 or b > 264)
+# 4 задание
+q = "Hom"
+w = "Hom"
+print(q == w)
+print(q != w)
