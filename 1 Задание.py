dbfbd94e3427e2ff0a9fcca04c88319801748aa4
Xevbyk <98812493+Xevbyk@users.noreply.github.com>
2023-02-14 09:20:17 +0300

    Классная работа, Рогожин Данил ИБС215

---
+++ 1 Задание.py
@@ -0,0 +1,12 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = 'No'
+big_netseloe = tseloe * 3.5
+drobnoe = drobnoe - 1
+tseloe / drobnoe
+big_netseloe / drobnoe
+stroka = stroka * 2 + "_" +"Yes" * 3
+print(stroka)
+print(big_netseloe)
+print(tseloe)
+print(drobnoe)
