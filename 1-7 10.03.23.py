d54e09d35f456236a4bb3f54c65b7506e8e565bc
Xevbyk <98812493+Xevbyk@users.noreply.github.com>
2023-03-10 15:12:01 +0300

    Домашняя работа 10.03.23

---
+++ 1-7 10.03.23.py
@@ -0,0 +1,44 @@
+#1
+a = int(input())
+b = int(input())
+if a > b:
+    print("Максимальное значение", a)
+    print("Минимальное значение", b)
+elif a < b:
+    print("Максимальное значение", b)
+    print("Минимальное значение", a)
+else:
+    print("a = b")
+
+#2
+a = int(input())
+b = int(input())
+c = b/2
+if a >= c :
+    print("Впишется")
+else:
+    print("Не впишется")
+    
+#4
+a = int(input())
+b = int(input())
+if a >= b :
+    print("Впишется")
+else:
+    print("Не впишется")
+    
+#5
+a = int(input())
+b = int(input())
+if a > b:
+    print("Большее значение", a)
+elif a < b:
+    print("Большее значение", b)
+    
+#7
+a = int(input())
+b = int(input())
+if a < b:
+    print("Меньшее значение", a)
+elif a > b:
+    print("Меньшее значение", b)
