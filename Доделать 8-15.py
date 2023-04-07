2f385586d0bba27f10313a8d2d9de70f30993d67
Xevbyk <98812493+Xevbyk@users.noreply.github.com>
2023-04-07 16:01:50 +0300

    №№№№№№№

---
+++ Доделать 8-15.py
@@ -0,0 +1,21 @@
+#6
+a = 0
+while a < 40:
+    a = a + 2
+    print(a)
+#7 
+a = 1
+i = 0
+while i < 15:
+    a = a + 2
+    i = i + 1
+    print(a)
+#8 
+a = 0
+b = 0
+i = 0
+while i < 20:
+    a = (b+2)**0.5
+    b = b + 2
+    i = i + 1
+    print(a)
