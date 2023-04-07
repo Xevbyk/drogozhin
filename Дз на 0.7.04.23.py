7e3ecfdf5d75918cdf4af72c7afd143edf0ee056
Xevbyk <98812493+Xevbyk@users.noreply.github.com>
2023-04-07 15:05:51 +0300

    Апрель ._. ._. ._. ._. ._. ._. ._. ._. ._. ._.

---
+++ Дз на 0.7.04.23.py
@@ -0,0 +1,30 @@
+a = 10 
+b = 25.4
+while a <= 22:
+    sant = a * b
+    print (sant)
+    a +=1 
+
+a = int(input())
+b = 1 
+while b <= 20:
+    rub = b * a
+    print(rub)
+    b+=1
+
+e = int(input())    
+i = 1 
+while i <= 20:
+    e **= i
+    print(e)
+    i += 1 
+
+i = 1.1 
+while i <= 21.1:
+    print (i)
+    i+=1    
+
+i = 2.1 
+while i <= 3:
+    print (i)
+    i+=0.1
